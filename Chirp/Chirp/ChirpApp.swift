a0a79578642ccb09a67b76fc8852d8b25bc32e24
Beth Lindenbaum <bethlindenbaum@comcast.net>
2025-11-17 04:02:14 +0100

    adding app to repo

---
+++ Chirp/Chirp/ChirpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChirpApp.swift
+//  Chirp
+//
+//  Created by Beth Lindenbaum on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ChirpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
