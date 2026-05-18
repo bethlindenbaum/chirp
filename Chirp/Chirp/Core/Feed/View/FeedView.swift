b382d4d31465bed7a65477d03a44d7e19683de99
Beth Lindenbaum <bethlindenbaum@comcast.net>
2026-05-18 19:29:51 -0400

    finished side menu view

--- Chirp/Chirp/Core/Feed/View/FeedView.swift
+++ Chirp/Chirp/Core/Feed/View/FeedView.swift
@@ -9,11 +9,13 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(0 ... 20, id: \.self) { _ in
-                    TweetRowView()
-                        .padding()
+        ZStack {
+            ScrollView {
+                LazyVStack {
+                    ForEach(0 ... 20, id: \.self) { _ in
+                        TweetRowView()
+                            .padding()
+                    }
                 }
             }
         }
